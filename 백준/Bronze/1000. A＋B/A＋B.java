3cc98320bb2565f0a5fedd19c11620dc69ef8664
김진근 <jindream6128@naver.com>
2023-07-02 15:33:35 +0900

    [Bronze V] Title: A+B, Time: 132 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int sum = a+b;
+
+        bw.write(String.valueOf(sum));
+
+        // io close
+        bw.close();
+        br.close();
+    }
+}
