ba4408949c61687b1bb9ce1023819621c3fc2c6c
김진근 <jindream6128@naver.com>
2023-07-02 15:38:08 +0900

    [Gold V] Title: 다이어트, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        //G 킬로그램
+        long g = Integer.parseInt(br.readLine());
+
+        long now =1;
+        long before =1;
+        while(now<=50_000){
+            if(((now*now)-(before*before))==g){
+                sb.append(now).append("\n");
+                before++;
+                now++;
+            }
+
+            if(((now*now)-(before*before))<g){
+                now++;
+            }else{
+                before++;
+            }
+        }
+        if(sb.length()==0){
+            sb.append(-1);
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
